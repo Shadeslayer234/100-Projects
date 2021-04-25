4cfb5e0613821a8b1a2ed06900cccc69a59b8305
Shadeslayer234 <praveen.weerabahu@gmail.com>
2021-04-25 10:39:11 +0530

    CSS completed

---
+++ Weather Application/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',()=>{
+  let long
+  let lat
+
+  if(navigator.geolocation){
+
+  }else{
+    h1.textContent = "This feature will not work with out the geolocation"
+  }
+})
